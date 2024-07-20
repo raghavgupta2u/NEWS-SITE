2c4ee608e8c32ed5678c63d576e4302097445954
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-07-21 00:30:08 +0530

    add

---
+++ admin/delete-user.php
@@ -0,0 +1,10 @@
+<?php
+if($_SESSION["user_role"] == '0'){
+    header("Location: http://localhost/news-template/admin/post.php");
+    }
+include 'config.php';
+$u_id = $_GET['id'];
+$sql = "DELETE FROM user WHERE user_id = {$u_id}";
+$result = mysqli_query($con,$sql);
+header("Location: http://localhost/news-template/admin/users.php");
+?>
---
+++ admin/logout.php
@@ -0,0 +1,9 @@
+<?php
+include "config.php";
+
+session_start();
+session_unset();
+session_destroy();
+
+header("Location: http://localhost/news-template/admin/index.php");
+?>
