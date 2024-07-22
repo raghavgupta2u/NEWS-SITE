8e26efd749a18561034c9fbd494693b83f590123
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-07-22 11:12:23 +0530

    add

--- admin/update-category.php
+++ admin/update-category.php
@@ -2,6 +2,19 @@
 if($_SESSION["user_role"] == '0'){
     header("Location: http://localhost/news-site-cms/NEWS-SITE/admin/post.php");
     } ?>
+ <?php
+                if (isset($_POST['submit'])) {
+                    include "config.php";
+                    $category = mysqli_real_escape_string($con, $_POST['cat_name']);
+                    $cat_id = mysqli_real_escape_string($con, $_POST['cat_id']);
+                 
+                        $sql = "UPDATE category SET category_name='{$category}' WHERE category_id= {$cat_id}";
+
+                        if (mysqli_query($con,$sql)) {
+                            header("location: http://localhost/news-site-cms/NEWS-SITE/admin/category.php");
+                        }
+                    }
+                ?>     
 
 <div id="admin-content">
     <div class="container">
@@ -14,12 +27,12 @@ if($_SESSION["user_role"] == '0'){
                 <?php
                 include "config.php";
                 $cat_id = $_GET['id'];
-                $sql = "SELECT * FROM category WHERE category_id = '{$cat_id}'";
+                $sql = "SELECT * FROM category WHERE category_id = {$cat_id}";
                 $result = mysqli_query($con, $sql);
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
                 ?>
-                        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
                             <div class="form-group">
                                 <input type="hidden" name="cat_id" class="form-control" value="<?php echo $row['category_id'] ?>" >
                             </div>
@@ -27,30 +40,10 @@ if($_SESSION["user_role"] == '0'){
                                 <label>Category Name</label>
                                 <input type="text" name="cat_name" class="form-control" value="<?php echo $row['category_name'] ?>" placeholder="" required>
                             </div>
-                            <input type="submit" name="sumbit" class="btn btn-primary" value="Update" required />
+                            <input type="submit" name="submit" class="btn btn-primary" value="Update" required />
                         </form>
 
-                <?php }
-                } ?>
-                 <?php
-                if (isset($_POST['submit'])) {
-                    include "config.php";
-                    $category = mysqli_real_escape_string($con, $_POST['cat_name']);
-                    $cat_id = mysqli_real_escape_string($con, $_POST['cat_id']);
-                    $sql = "SELECT category_name FROM category WHERE category_name='{$category}' AND NOT category_id='{$cat_id}'";
-                    $result1 = mysqli_query($con, $sql);
-                    if (mysqli_num_rows($result1) > 0) {
-                       echo "<p style = 'color:red;text-align:center;margin: 10px 0';> Category Name '" . $category . "' already exists.</p>";
-                    } else {
-                        $sql1 = "UPDATE category SET category_id='{$_POST['cat_id']}', category_name='{$_POST['cat_name']}'  WHERE  category_id={$_POST['cat_id']}";
-
-                        if (mysqli_query($con, $sql1)) {
-                            
-                            header("location: http://localhost/news-site-cms/NEWS-SITE/admin/category.php");
-                        }
-                    }
-                }
-                ?> 
+                <?php } } ?>
             </div>
         </div>
     </div>
