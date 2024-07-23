50891e3b5b8814214cf7d0821f455f64297cbfb2
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-07-24 00:03:25 +0530

    add

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,7 +1,10 @@
 <?php
 include 'config.php';
 $p_id = $_GET['id'];
-$sql = "DELETE FROM post WHERE post_id = {$p_id}";
-$result = mysqli_query($con,$sql);
+$c_id = $_GET['cat_id'];
+$sql = "DELETE FROM post WHERE post_id = {$p_id};";
+$sql .= "UPDATE category SET post = post - 1 WHERE category_id = {$c_id}";
+$result = mysqli_multi_query($con,$sql);
+
 header("Location: http://localhost/news-site-cms/NEWS-SITE/admin/post.php");
 ?>
