d98e4f4014741b42aef9a59e21073ce4ddec52ff
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-07-29 00:39:20 +0530

    add

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -2,6 +2,13 @@
 include 'config.php';
 $p_id = $_GET['id'];
 $c_id = $_GET['cat_id'];
+
+$sql1 = "SELECT* FROM post WHERE post_id = {$p_id};";
+$result1 = mysqli_query($con,$sql1);
+$row =mysqli_fetch_assoc($result1);
+
+//unlink("admin/upload/".$row['post_img']);
+
 $sql = "DELETE FROM post WHERE post_id = {$p_id};";
 $sql .= "UPDATE category SET post = post - 1 WHERE category_id = {$c_id}";
 $result = mysqli_multi_query($con,$sql);
